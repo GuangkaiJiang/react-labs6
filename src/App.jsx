e814e1019105594f70f0c8194c9c0c7eacde5bc1
Guangkai Jiang <skyrimtravel@outlook.com>
2019-11-19 13:30:20 +0100

    LAB6EX1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-const App = () => (
-  <h1>Minimal React</h1>  
-)
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      employees:[],
+      isLoading:true,
+      employeesAreFetched:false,
+      open:false
+    }
+  }
+  componentDidMount(){
+    fetch('http://localhost:3004/employees')
+    .then(response => response.json())
+    .then(data=>this.setState({employees:data}))
+    .then(()=>this.setState({employeesAreFetched:true}))
+    .then(()=>this.setState({isLoading:false}));
+  }
+render(){
+  console.log('render was fired');
+  return (
+    <div>
+    <>{this.state.isLoading?"Loading...":("Loaded:"+this.state.employees.length.toString())}</>
+    </div>
+  )
+}
+}
 
 export default App
